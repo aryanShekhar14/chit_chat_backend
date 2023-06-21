b0c3f87823f50489f8c5603cd02212c757bd479b
Aryan Shekhar <99390142+aryanShekhar14@users.noreply.github.com>
2023-06-21 18:55:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,9 +39,6 @@ const server = app.listen(5000, console.log(`Server started on port ${PORT}`.yel
 
 const io = require('socket.io')(server, {
     pingTimeout: 60000, //close connection after 60 sec if no response to save bandwidth
-    cors: {
-        origin: "http://localhost:3000"
-    }
 })
 
 io.on("connection", (socket) => {
